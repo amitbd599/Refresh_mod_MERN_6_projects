0c9932235961459a9b9a82e5b72e41ef7f1fa8b1
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-29 15:19:09 +0600

    update data

--- Payment Gateway Implementation and One to One Topic/app/controllers/InvoiceController.js
+++ Payment Gateway Implementation and One to One Topic/app/controllers/InvoiceController.js
@@ -1,4 +1,4 @@
-import { CreateInvoiceService } from "../service/InvoiceService.js";
+import { CreateInvoiceService, PaymentCancelService, PaymentFailService, PaymentIPNService, PaymentSuccessService } from "../service/InvoiceService.js";
 
 
 
@@ -11,25 +11,25 @@ export const CreateInvoice = async (req, res) => {
 
 //! Payment Fail 
 export const PaymentFail = async (req, res) => {
-    // let result = await PaymentFailService(req);
+    let result = await PaymentFailService(req);
     res.redirect(process.env.Origin_HOST_Site + "/profile");
 };
 
 
 //! Payment Cancel
 export const PaymentCancel = async (req, res) => {
-    // let result = await PaymentCancelService(req);
+    let result = await PaymentCancelService(req);
     res.redirect(process.env.Origin_HOST_Site + "/profile");
 };
 
 //!   Payment IPN 
 export const PaymentIPN = async (req, res) => {
-    // let result = await PaymentIPNService(req);
+    let result = await PaymentIPNService(req);
     res.redirect(process.env.Origin_HOST_Site + "/profile");
 };
 
 //!   Payment Success
 export const PaymentSuccess = async (req, res) => {
-    // let result = await PaymentSuccessService(req);
+    let result = await PaymentSuccessService(req);
     res.redirect(process.env.Origin_HOST_Site + "/profile");
 };
--- Payment Gateway Implementation and One to One Topic/app/service/InvoiceService.js
+++ Payment Gateway Implementation and One to One Topic/app/service/InvoiceService.js
@@ -1,10 +1,33 @@
-
+import axios from "axios"
 
 // 1- developer sslcommerz
 
 
 export const CreateInvoiceService = async (req) => {
     try {
+        // working ................
+
+        let tran_id = Math.floor(10000000000 + Math.random() * 900000000);
+        let cus_name = "Alex";
+        let cus_email = "Alex@gmail.com";
+        let cus_add = "House no 42, Dhaka";
+        let cus_add2 = "House no 42, Dhaka";
+        let cus_city = "Dhaka";
+        let cus_state = "Dhaka x";
+        let cus_postcode = "2035";
+        let cus_country = "Bangladesh";
+        let cus_phone = "01814331350";
+
+        // Shipping
+        let ship_name = "Alex";
+        let ship_add = "House no 42, Dhaka";
+        let ship_add2 = "House no 42, Dhaka";
+        let ship_city = "Dhaka";
+        let ship_state = "Dhaka x";
+        let ship_postcode = "2035";
+        let ship_country = "Bangladesh";
+        let ship_phone = "01814331350";
+
         let PaymentSetting = {
             store_id: "theme664dfb04bfaf4",
             store_passwd: "theme664dfb04bfaf4@ssl",
@@ -12,7 +35,7 @@ export const CreateInvoiceService = async (req) => {
             success_url: "http://localhost:5000/api/payment-success",
             fail_url: "http://localhost:5000/api/payment-fail",
             cancel_url: "http://localhost:5000/api/payment-cancel",
-            ipn_url: "http://localhost:5000/api",
+            ipn_url: "http://localhost:5000/api/payment-ipn",
             init_url: "https://sandbox.sslcommerz.com/gwprocess/v3/api.php",
         };
 
@@ -20,7 +43,7 @@ export const CreateInvoiceService = async (req) => {
         // Request Parameters
         form.append("store_id", PaymentSetting.store_id);
         form.append("store_passwd", PaymentSetting.store_passwd);
-        form.append("total_amount", totalPayable.toString());
+        form.append("total_amount", 2000);
         form.append("currency", PaymentSetting.currency);
         form.append("tran_id", tran_id);
         form.append("success_url", `${PaymentSetting.success_url}/${tran_id}`);
@@ -29,26 +52,26 @@ export const CreateInvoiceService = async (req) => {
         form.append("ipn_url", `${PaymentSetting.ipn_url}/${tran_id}`);
 
         // Customer Information
-        form.append("cus_name", profile[0]?.cus_name);
+        form.append("cus_name", cus_name);
         form.append("cus_email", cus_email);
-        form.append("cus_add1", profile[0]?.cus_add);
-        form.append("cus_add2", profile[0]?.cus_add);
-        form.append("cus_city", profile[0]?.cus_city);
-        form.append("cus_state", profile[0]?.cus_state);
-        form.append("cus_postcode", profile[0]?.cus_postcode);
-        form.append("cus_country", profile[0]?.cus_country);
-        form.append("cus_phone", profile[0]?.cus_phone);
+        form.append("cus_add1", cus_add);
+        form.append("cus_add2", cus_add2);
+        form.append("cus_city", cus_city);
+        form.append("cus_state", cus_state);
+        form.append("cus_postcode", cus_postcode);
+        form.append("cus_country", cus_country);
+        form.append("cus_phone", cus_phone);
 
         // Shipment Information
         form.append("shipping_method", "YES");
-        form.append("ship_name", profile[0]?.ship_name);
-        form.append("ship_add1", profile[0]?.ship_add);
-        form.append("ship_add2", profile[0]?.ship_add);
-        form.append("ship_city", profile[0]?.ship_city);
-        form.append("ship_state", profile[0]?.ship_state);
-        form.append("ship_country", profile[0]?.ship_country);
-        form.append("ship_postcode", profile[0]?.ship_postcode);
-        form.append("ship_phone", profile[0]?.ship_phone);
+        form.append("ship_name", ship_name);
+        form.append("ship_add1", ship_add);
+        form.append("ship_add2", ship_add2);
+        form.append("ship_city", ship_city);
+        form.append("ship_state", ship_state);
+        form.append("ship_country", ship_country);
+        form.append("ship_postcode", ship_postcode);
+        form.append("ship_phone", ship_phone);
 
         // Product Information
         form.append("product_name", "According Invoice");
